f72d611161fb43f77bbacf07cfd664924c61bd9e
psfedo3 <aminuangela55@gmail.com>
2020-10-09 07:03:08 +0100

    fix js

--- js/registration-form.js
+++ js/registration-form.js
@@ -3,14 +3,9 @@ let inputField = document.querySelector('.form__field__input');
 inputField.addEventListener('input', function(event) {
   var floated = document.querySelectorAll('.form__field--floating');
   for (i = 0; i < floated.length; i++) {
-    floated.classList.add('is-active');
+    floated[i].classList.add('is-active');
   }
 })
-
-
-
-
-
 
 
 function previewImage(event) {
