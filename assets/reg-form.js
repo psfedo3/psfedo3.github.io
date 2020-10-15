19d75df9ba69c85bd72814d95a260ad5a4733061
psfedo3 <aminuangela55@gmail.com>
2020-10-15 13:28:49 +0100

    form

--- assets/reg-form.js
+++ assets/reg-form.js
@@ -48,3 +48,26 @@
        $('body').addClass('show-modal');
      }
    })(jQuery);
+   
+   
+  // some styles for the form
+  
+  let inputField = document.querySelector('.form__field__input');
+  inputField.addEventListener('input', function(event) {
+    var floated = document.querySelectorAll('.form__field--floating');
+    for (i = 0; i < floated.length; i++) {
+      floated[i].classList.add('is-active');
+    }
+  })
+  
+  
+  function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      var outputImage = document.querySelector('.js-image-upload');
+      outputImage.src = reader.result;
+      var uploadFile = document.querySelector(".js-upload");
+      uploadFile.value = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+  }
