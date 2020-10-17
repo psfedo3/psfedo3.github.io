a296f45a237d8093fa71ca333612cd0fafad2ce8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-17 04:27:48 +0000

    Deploy psfedo3/psfedo3.github.io to github.com/psfedo3/psfedo3.github.io.git:master

--- assets/reg-form.js
+++ assets/reg-form.js
@@ -52,14 +52,53 @@
    
    
   // image preview
-  
-  function previewImage(event) {
+  function previewImage(evt) {
+    var filesToUploads = document.getElementById('file').files;
+        var file = filesToUploads[0];
+        if (file) {
+
     var reader = new FileReader();
-    reader.onload = function() {
-      var outputImage = document.querySelector('.js-image-upload');
-      outputImage.src = reader.result;
-      var avatar = document.querySelector('#js-image-avatar');
-      avatar.value = reader.result;
-    }
-    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = function(e) {
+      document.getElementById('previewImg').src = e.target.result;
+     var img = document.createElement("img");
+     img.src = e.target.result;
+     
+     var canvas = document.createElement("canvas");
+     var ctx = canvas.getContext("2d");
+     ctx.drawImage(img, 0, 0);
+     
+     var MAX_WIDTH = 400;
+     var MAX_HEIGHT = 400;
+     var width = img.width;
+     var height = img.height;
+     
+     if (width > height) {
+       if (width > MAX_WIDTH) {
+         height *= MAX_WIDTH / width;
+         width = MAX_WIDTH;
+       }
+     } else {
+       if (height > MAX_HEIGHT) {
+         width *= MAX_HEIGHT / height;
+         height = MAX_HEIGHT;
+       }
+     }
+     canvas.width = width;
+     canvas.height = height;
+     var ctx = canvas.getContext("2d");
+     ctx.drawImage(img, 0, 0, width, height);
+     
+     dataurl = canvas.toDataURL(file.type);
+     var avatar = document.querySelector('#js-image-avatar');
+      avatar.value = dataurl;
+     
+    };
+    reader.readAsDataURL(file);
   }
+  
+    }
+  
+ 
+  
+      
+  
